5aa02c7850bca5e8221c0fb708396f05ba284467
Josh Cullen <joshcullen10@gmail.com>
2021-02-04 09:28:13 -0500

    Update movescape plots from average to seasonal estimates

--- Compare Time vs SSF_giantarm.R
+++ Compare Time vs SSF_giantarm.R
@@ -36,41 +36,42 @@ resist.pop2<- read.csv("Giant Armadillo Resistance summary results.csv", as.is =
 
 ### Plot Velocity-based Resistance vs Habitat Selection
 
-dat.comp<- data.frame(sel = ssf.pop2$mu, res = resist.pop2$mu,
-                      green = raster::values(mean(green, na.rm = T)),
-                      wet = raster::values(mean(wet, na.rm = T)))
+dat.comp<- cbind(ssf.mean.id3, time = resist.mean.id3$time)
 
-quant_99.9<- quantile(dat.comp$res, 0.99)
+quant_99.9<- quantile(na.omit(dat.comp$time), 0.999)
 dat.comp2<- dat.comp %>% 
-  filter(res <= quant_99.9)
-time.mid<- (max(dat.comp2$res, na.rm = T) + min(dat.comp2$res, na.rm = T))/2
+  filter(time <= quant_99.9)
+time.mid<- (max(dat.comp2$time, na.rm = T) + min(dat.comp2$time, na.rm = T))/2
 
-ggplot(dat.comp2, aes(res, sel, color = green)) +
+
+
+ggplot(dat.comp, aes(time, sel, color = season)) +
+  geom_point(size = 1, alpha = 0.25) +
   geom_hline(aes(yintercept = 0.5)) +
   geom_vline(aes(xintercept = time.mid)) +
-  scale_color_distiller("Greenness", palette = "Greens", direction = 1) +
-  geom_point(alpha = 0.5, na.rm = T) +
   theme_bw() +
   ylim(0,1) +
+  xlim(0, max(dat.comp$time, na.rm = T)) +
   labs(x= "Time (min)", y="Habitat Preference") +
   theme(axis.title = element_text(size = 18),
-        axis.text = element_text(size = 10))
+        axis.text = element_text(size = 10)) +
+  facet_wrap(~ season, nrow = 2)
 
 # ggsave("Giant Armadillo Quadrant_lulc.png", width = 7, height = 5,
 #        units = "in", dpi = 300)
 
 
 
-ggplot(dat.comp2, aes(res, sel, color = wet)) +
-  geom_hline(aes(yintercept = 0.5)) +
-  geom_vline(aes(xintercept = time.mid)) +
-  scale_color_distiller("Wetness", palette = "Blues", direction = 1) +
-  geom_point(na.rm = T) +
-  theme_bw() +
-  ylim(0,1) +
-  labs(x= "Time (min)", y="Habitat Preference") +
-  theme(axis.title = element_text(size = 18),
-        axis.text = element_text(size = 10))
+# ggplot(dat.comp2, aes(res, sel, color = wet)) +
+#   geom_hline(aes(yintercept = 0.5)) +
+#   geom_vline(aes(xintercept = time.mid)) +
+#   scale_color_distiller("Wetness", palette = "Blues", direction = 1) +
+#   geom_point(na.rm = T) +
+#   theme_bw() +
+#   ylim(0,1) +
+#   labs(x= "Time (min)", y="Habitat Preference") +
+#   theme(axis.title = element_text(size = 18),
+#         axis.text = element_text(size = 10))
 
 # ggsave("Giant Armadillo Quadrant_ndwi.png", width = 7, height = 5,
 #        units = "in", dpi = 300)
@@ -84,11 +85,11 @@ ggplot(dat.comp2, aes(res, sel, color = wet)) +
 
 ## Using 2 continuous color scales
 ggplot() +
-  geom_raster(data = resist.pop2, aes(x, y, fill = mu), alpha = 0.75) +
+  geom_raster(data = dat.comp, aes(x, y, fill = time), alpha = 0.75) +
   scale_fill_gradient("Time Spent\nper Cell (min)", low = "white", high = "royalblue3",
-                       na.value = "transparent", limits = c(0,10)) +
+                       na.value = "transparent", limits = c(0,quant_99.9)) +
   new_scale_fill() +
-  geom_raster(data = ssf.pop2, aes(x, y, fill = mu), alpha = 0.75) +
+  geom_raster(data = dat.comp, aes(x, y, fill = sel), alpha = 0.75) +
   scale_fill_gradient("Selection", low = "red4", high = "white",
                        na.value = "transparent", limits = c(0,1)) +
   geom_path(data = dat, aes(x, y, group = id), alpha = 0.5, color = "black") +
@@ -103,7 +104,8 @@ ggplot() +
         strip.text = element_text(size = 16, face = "bold"),
         plot.title = element_text(size = 22),
         legend.title = element_text(size = 14),
-        legend.text = element_text(size = 12))
+        legend.text = element_text(size = 12))  +
+  facet_wrap(~ season)
 
 # ggsave("Giant Armadillo Connectivity_cont.png", width = 7, height = 5,
 #        units = "in", dpi = 300)
@@ -116,7 +118,7 @@ ggplot() +
 all.dat<- cbind(resist.pop2[,-4], ssf.pop2$mu)
 names(all.dat)[3:4]<- c("time", "sel")
 
-all.dat<- all.dat %>% 
+all.dat<- dat.comp %>% 
   mutate(fun_class = case_when(.$time > time.mid & .$sel > 0.5 ~ "Slow-Preferred",
                                 .$time > time.mid & .$sel < 0.5 ~ "Slow-Avoided",
                                 .$time < time.mid & .$sel > 0.5 ~ "Fast-Preferred",
@@ -140,7 +142,8 @@ ggplot() +
         strip.text = element_text(size = 16, face = "bold"),
         plot.title = element_text(size = 22),
         legend.title = element_text(size = 14),
-        legend.text = element_text(size = 12))
+        legend.text = element_text(size = 12)) +
+  facet_wrap(~ season, nrow = 2)
 
 # ggsave("Giant Armadillo Connectivity_disc.png", width = 7, height = 5,
 #        units = "in", dpi = 300)
